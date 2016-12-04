12bd5c4a99f739de25140a014c4662666cfe9c7f
Yurick <yurick.hauschild@gmail.com>
2016-12-03 22:44:59 -0200

    Started working in solveSystem

--- parte1/q1.f90
+++ parte1/q1.f90
@@ -2,8 +2,8 @@ program ajuste1
 
     implicit none
 
-    integer :: ios, i, rank
-    integer, parameter :: N = 17
+    integer :: ios, i, j, rank
+    integer, parameter :: N = 17 !datasheet size
     double precision :: sumx, sumy
     double precision, dimension(N) :: x, y
 
@@ -11,15 +11,27 @@ program ajuste1
     ! but the default value, if you don't explicit it?
     open(unit=100, file="tabela1.dat", iostat=ios, action="read")
     if ( ios /= 0 ) stop "Erro na leitura do arquivo 100."
-
     do i=1, N, 1
         read(100, *) x(i), y(i)
-        sumx = sumx + x(i)
-        sumy = sumy + y(i)
     end do
-
-    
-
     close(unit=100)
+
+    read(*,*) rank
+
+contains
+
+    ! solves system Ax=B, dumps answer in x
+    subroutine solveSystem(A, x, B, i, n)
+        implicit none
+
+        integer, intent(in) :: i, n
+
+        double precision, dimension(i, n), intent(in) :: A
+        double precision, dimension(n), intent(in) :: B
+        double precision, dimension(i), intent(out) :: x
+
+        
+
+    end subroutine
 
 end program ajuste1
